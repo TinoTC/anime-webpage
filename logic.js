6b66150fff9960ed4396cecad9e062ae70f14ccb
Tino Carrier <86430901+TinoTC@users.noreply.github.com>
2021-07-14 09:07:10 -0500

    Delete logic.js

--- logic.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("motivate").addEventListener("click", unhide)
-function unhide() {
-    document.getElementById("inspiring").style.opacity=1;
-}
-
-document.getElementById("unmotivate").addEventListener("click", hide)
-function hide() {
-    document.getElementById("inspiring").style.opacity=0;
-}
